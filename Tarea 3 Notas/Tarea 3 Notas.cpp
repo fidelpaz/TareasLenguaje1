cb1d2fa3d484f5dc0c2ec9979c1a8940e1eab863
fidelpaz <fidelpg2001@gmail.com>
2022-02-08 22:13:07 -0600

    Tarea 3 ejercicio de notas

---
+++ Tarea 3 Notas/Tarea 3 Notas.cpp
@@ -0,0 +1,134 @@
+/*
+  	Propósito: Programa notas que cumpla con la tabla de wikipedia
+  
+  	Autor: Fidel Alfredo Paz Gómez
+  	Fecha: 07/02/22
+	Hora: 21:44
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+ 	setlocale(LC_CTYPE, "Spanish");
+ 	cout.precision(8);
+ 	
+	int nota = 0;
+	
+	cout << " Ingrese la nota que obtuvo: ";
+	cin >> nota;
+	
+	if ( nota >= 96 && nota <= 100 )
+	{
+		cout << " Obtuviste una A++";
+	}
+ 	else
+ 		if ( nota >= 91 && nota <= 95)
+ 		{
+			cout << " Obtuviste una A+";
+		}
+		else
+			if ( nota >= 86 && nota <= 90 )
+			{
+				cout << " Obtuviste una A";
+			}
+			else
+				if ( nota >= 81 && nota <= 85 )
+				{
+					cout << " Obtuviste una A-";
+				}
+				else
+					if ( nota >= 76 && nota <= 80 )
+					{
+						cout << " Obtuviste una B+";
+					}
+					else
+						if ( nota >= 71 && nota <= 75 )
+						{
+							cout << " Obtuviste una B";
+						}
+						else
+							if ( nota >= 66 && nota <= 70 )
+							{
+								cout << " Obtuviste una B-";
+							}
+							else
+								if ( nota >= 61 && nota <= 65 )
+								{
+									cout << " Obtuviste una C+";
+								}
+								else
+									if ( nota >= 56 && nota <= 60 )
+									{
+										cout << " Obtuviste una C";
+									}
+									else
+										if ( nota >= 51 && nota <= 55 )
+										{
+											cout << " Obtuviste una C-";
+										}
+										else
+											if ( nota >= 46 && nota <= 50 )
+											{
+												cout << " Obtuviste una D+";
+											}
+											else
+												if ( nota >= 41 && nota <= 45 )
+												{
+													cout << " Obtuviste una D";
+												}
+												else
+													if ( nota >= 36 && nota <= 40 )
+													{
+														cout << " Obtuviste una D-";
+													}
+													else
+														if ( nota >= 31 && nota <= 35 )
+														{
+															cout << " Obtuviste una E+";
+														}
+														else
+															if ( nota >= 26 && nota <= 30 )
+															{
+																cout << " Obtuviste una E";
+															}
+															else
+																if ( nota >= 21 && nota <= 25 )
+																{
+																	cout << " Obtuviste una E-";
+																}
+																else
+																	if ( nota >= 16 && nota <= 20 )
+																	{
+																		cout << " Obtuviste una F+";
+																	}
+																	else
+																		if ( nota >= 11 && nota <= 15 )
+																		{
+																			cout << " Obtuviste una F";
+																		}
+																		else
+																			if ( nota >= 6 && nota <= 10 )
+																			{
+																				cout << " Obtuviste una F";
+																			}
+																			else
+																				if ( nota >= 1 && nota <= 5 )
+																				{
+																					cout << " Obtuviste una F-";
+																				}
+																				else
+																					if ( nota <= 0 && nota < 4 )
+																					{
+																						cout << " Obtuviste una F--";
+																					}
+																					else
+																					{
+																						cout << " Ingrese una nota entre 0 - 100 ";
+																					}
+ 	
+ 	return 0;
+}
