4154c9ae180814b8a40aef134664f138c2003c59
fidelpaz <fidelpg2001@gmail.com>
2022-02-11 22:16:45 -0600

    Haciendo un hola mundo

---
+++ HolaMundo/HolaMundo.cpp
@@ -0,0 +1,22 @@
+/*
+  	Propósito: Haciendo un hola mundo
+  
+  	Autor: Fidel Alfredo Paz Gomez
+  	Fecha: 11/02/22
+	Hora: 10:15
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+ 	setlocale(LC_CTYPE, "Spanish");
+ 	cout.precision(12);
+ 	
+ 	cout << " Hola Mundo ";
+ 	
+ 	return 0;
+}
