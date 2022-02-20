dcccdb34f33a24e85ad5b75ddb881e0d969597e0
fidelpaz <fidelpg2001@gmail.com>
2022-02-19 23:33:33 -0600

    Ejercicio con ciclo for

---
+++ Ciclos For/Ciclos For.cpp
@@ -0,0 +1,26 @@
+/*
+  	Propósito: Ejercicio con Ciclo For
+  
+  	Autor: Fidel Alfredo Paz Gomez
+  	Fecha: 19/02/22
+	Hora: 23:35
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+ 	setlocale(LC_CTYPE, "Spanish");
+ 	cout.precision(12);
+ 	
+ 	for ( int indice = 1; indice <= 10; indice++ )
+ 	{
+		cout << " Valor de la variable indice " << indice << endl;
+	}
+	
+	
+	return 0;
+}
