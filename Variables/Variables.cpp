af21df51d3adc13225bbd6667d838554f6ce0d67
fidelpaz <fidelpg2001@gmail.com>
2022-02-20 22:25:58 -0600

    Ejercicio de variables

---
+++ Variables/Variables.cpp
@@ -0,0 +1,30 @@
+/*
+  	Propósito: Ejercicio de variables
+  
+  	Autor: Fidel Alfredo Paz Gomez
+  	Fecha: 20/02/22
+	Hora: 22:26
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+ 	setlocale(LC_CTYPE, "Spanish");
+ 	cout.precision(12);
+ 	
+ 	int numeroEntero;
+ 	numeroEntero = 15;
+ 	
+ 	cout << " Entero: " << numeroEntero << endl;
+ 	
+ 	numeroEntero = 18;
+ 	
+ 	cout << " Entero modificado: " << numeroEntero;
+ 	
+ 	return 0;
+}
+
