17147be1e5876f09f5a0cd236cdc9ef3691fa80d
fidelpaz <fidelpg2001@gmail.com>
2022-04-02 22:39:45 -0600

    Ejercicio 32 Calculadora con Funciones trwon

---
+++ Calculadora con Funciones trwon/Calculadora con Funciones trwon.cpp
@@ -0,0 +1,59 @@
+/*
+  	Propósito: Ejercicio 32 Calculadora con Funciones trwon
+  
+  	Autor: Fidel Paz
+  	Fecha:
+	Hora:
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int sumar (int a, int b)
+{
+	return a + b;
+}
+
+int calculadora(int a, int b, char operador)
+{
+	if(operador == '+')
+	{
+		return sumar(a, b);
+	}
+	
+	throw " El operador no existe ";
+}
+
+int main()
+{
+ 	setlocale(LC_CTYPE, "Spanish");
+ 	cout.precision(12);
+ 	
+	int n1 = 0;
+	int n2 = 0;
+	int resultado = 0;
+	char operador;
+	
+	cout << " Ingrese a: ";
+	cin >> n1;
+ 	cout << " Ingrese b: ";
+	cin >> n2;
+	cout << " Ingrese el operador (+, -, *, /): ";
+	cin >> operador;
+ 	
+ 	cout << endl;
+	
+	try
+	{
+		resultado = calculadora(n1, n2, operador);
+		cout << " Resultado de " << n1 << " " << operador << " " << n2 << " es ";
+		cout << calculadora(n1, n2, operador);
+	}
+	catch(const char* error)
+	{
+		cout << error;
+	}
+ 	return 0;
+}
