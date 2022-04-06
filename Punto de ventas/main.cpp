7060a7a1d29da332d5bda94d5e2e0cc1398aaedb
fidelpaz <fidelpg2001@gmail.com>
2022-04-05 21:33:47 -0600

    Ejercicio 40 punto de ventas

---
+++ Punto de ventas/main.cpp
@@ -0,0 +1,24 @@
+/*
+  	Propósito:  Ejercicio 40 punto de ventas
+  
+  	Autor: Fidel Paz 
+  	Fecha: 
+	Hora: 
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+extern void menu();
+
+int main()
+{
+ 	setlocale(LC_CTYPE, "Spanish");
+ 	cout.precision(12);
+ 	
+ 	menu();
+ 	
+ 	return 0;
+}
