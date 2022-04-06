4660f01edbb43c73ae48d572a4f80c8928e07269
fidelpaz <fidelpg2001@gmail.com>
2022-04-05 22:02:48 -0600

    Ejercicio 42 punto de ventas parte 3

---
+++ Punto de ventas 3/factura.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+using namespace std;
+
+double subTotal;
+string listaProductos;
+
+void agregarProducto(string descripcion, int cantidad, double precio)
+{
+	listaProductos = listaProductos + descripcion + '\n';
+	subTotal = subTotal + (cantidad * precio);
+}
