a9400ca9edcf5c1d72b9afc14c002b721c034436
fidelpaz <fidelpg2001@gmail.com>
2022-04-05 22:14:59 -0600

    Ejercicio 43 punto de ventas parte 4

---
+++ Punto de ventas 4/main.cpp
@@ -0,0 +1,26 @@
+/*
+  	Propósito:  Ejercicio 43 punto de ventas parte 4
+  
+  	Autor: Fidel Paz 
+  	Fecha: 
+	Hora: 
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+extern void menu();
+
+int main()
+{
+ 	setlocale(LC_CTYPE, "Spanish");
+ 	cout.precision(12);
+ 	
+ 	menu();
+ 	
+ 	
+ 	
+ 	return 0;
+}
